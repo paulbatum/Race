c217a9a80f0c30452ee37ab29e58a0ca346958ac
Paul Batum <paul.batum@gmail.com>
2009-05-05 23:49:47 +1000

    Commented out all the modelling and started again with TDD

---
+++ Race.Domain/Hack1.cs
@@ -0,0 +1,101 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Race.Domain
+//{
+//    public abstract class GamePhase
+//    {
+//        public void Execute(IEnumerable<PhaseParticipation> participations)
+//        {
+//            foreach(var participation in participations)
+//            {
+//                //participation.Player.DrawAndDiscardCards
+//                DoPhaseAction(participation);
+//            }
+//        }
+
+//        protected abstract void DoPhaseAction(PhaseParticipation participation);
+//    }
+
+//    public class PhaseParticipation
+//    {
+//    }
+
+//    public class ExplorePhase : GamePhase
+//    {
+//        protected override void DoPhaseAction(PhaseParticipation participation)
+//        {
+            
+//        }
+//    }
+
+//    public abstract class PhaseSelection
+//    {
+//        public abstract GamePhase Phase { get; }
+//        public abstract SelectionBonus Bonus { get; }
+//    }
+
+//    public class ExploreDrawThreeKeepTwo : PhaseSelection
+//    {
+//        public override GamePhase Phase
+//        {
+//            get { return GamePhase.Explore; }
+//        }
+
+//        public void Execute(bool withBonus)
+//        {
+            
+//        }
+//    }
+    
+
+//    public abstract class SelectionBonus
+//    {
+//    }
+
+//    public class GameRound
+//    {
+//        private readonly IEnumerable<Player> _players;
+
+//        public GameRound(IEnumerable<Player> players)
+//        {
+//            _players = players;
+//        }
+
+//        public void Begin()
+//        {
+//            var selections = _players.Select(player => player.GetPhaseSelection());
+//            var phases = selections
+//                .GroupBy(selection => selection.Phase);
+                
+                
+
+
+
+//        }
+//    }
+
+//    public class Player
+//    {
+//        public Hand Hand { get; private set; }
+
+//        public PhaseSelection GetPhaseSelection()
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+
+//    public class Hand
+//    {
+        
+//    }
+
+//    public class Card
+//    {
+        
+//    }
+
+
+//}
---
+++ Race.Tests/RaceTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Race.Tests
+{
+    [TestFixture]
+    public class RaceTests
+    {
+        [Test]
+        public void Can_create_game_with_two_players()
+        {
+            var game = new Game();
+            game.AddPlayer(new Player {Name = "Paul"});
+            game.AddPlayer(new Player {Name = "Tristan"});
+
+            game.Players.ShouldHaveCount(2);
+
+        }
+    }
+
+    public class Player
+    {
+        public string Name { get; set; }
+    }
+
+    public class Game
+    {
+        private IEnumerable<Player> _players;
+
+        public void AddPlayer(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Player> Players
+        {
+            get { return _players; }
+        }
+    }
+}
